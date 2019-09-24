8db4d53f949840be82293c3b8e4f0122c57aae1c
Gabriel Cunha <gn_cunha@hotmail.com>
2019-09-24 20:37:08 -0300

    Listando/Excluindo produtos

--- app/src/main/java/gabrielcunha/cursoandroidyoutubeapp/ifood/activity/activity/EmpresaActivity.java
+++ app/src/main/java/gabrielcunha/cursoandroidyoutubeapp/ifood/activity/activity/EmpresaActivity.java
@@ -3,6 +3,8 @@ package gabrielcunha.cursoandroidyoutubeapp.ifood.activity.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 import android.content.Intent;
@@ -10,15 +12,35 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import gabrielcunha.cursoandroidyoutubeapp.ifood.R;
+import gabrielcunha.cursoandroidyoutubeapp.ifood.activity.adapter.AdapterProduto;
 import gabrielcunha.cursoandroidyoutubeapp.ifood.activity.helper.ConfiguracaoFirebase;
+import gabrielcunha.cursoandroidyoutubeapp.ifood.activity.helper.UsuarioFirebase;
+import gabrielcunha.cursoandroidyoutubeapp.ifood.activity.listener.RecyclerItemClickListener;
+import gabrielcunha.cursoandroidyoutubeapp.ifood.activity.model.Produto;
 
 public class EmpresaActivity extends AppCompatActivity {
 
     private FirebaseAuth autenticacao;
+    private RecyclerView recyclerProdutos;
+    private AdapterProduto adapterProduto;
+    private List<Produto> produtos = new ArrayList<>();
+    private DatabaseReference firebaseRef;
+    private String idUsuarioLogado ="";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +48,84 @@ public class EmpresaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_empresa);
 
         autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        recyclerProdutos = findViewById(R.id.recyclerProdutos);
+
+        firebaseRef = ConfiguracaoFirebase.getFirebase();
+        idUsuarioLogado = UsuarioFirebase.getIdUusuario();
 
         //Configurações Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Ifood - empresa");
         setSupportActionBar(toolbar);
+
+        //Configurar adapter
+
+        adapterProduto = new AdapterProduto(produtos,this);
+
+        //Configurar RecyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerProdutos.setLayoutManager(layoutManager);
+        recyclerProdutos.setHasFixedSize(true);
+        recyclerProdutos.setAdapter(adapterProduto);
+
+        //Recupera produtos para empresa
+        recuperaProdutos();
+
+        //Adicionar Evento de click no recyclerView
+        recyclerProdutos.addOnItemTouchListener(
+                new RecyclerItemClickListener(
+                        this,
+                        recyclerProdutos,
+                        new RecyclerItemClickListener.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+
+                            }
+
+                            @Override
+                            public void onLongItemClick(View view, int position) {
+                                Produto produtoSelecionado = produtos.get(position);
+                                produtoSelecionado.remover();
+                                Toast.makeText(EmpresaActivity.this,
+                                        "Produto excluído com sucesso!",
+                                        Toast.LENGTH_SHORT)
+                                        .show();
+                            }
+
+                            @Override
+                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                            }
+                        }
+                )
+        );
+
+
+    }
+
+    private void recuperaProdutos() {
+
+        DatabaseReference produtosRef = firebaseRef
+                .child("produtos")
+                .child(idUsuarioLogado);
+
+        produtosRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                produtos.clear();
+
+                for(DataSnapshot ds:dataSnapshot.getChildren()){
+                    produtos.add(ds.getValue(Produto.class));
+
+                }
+                adapterProduto.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
 
     }
