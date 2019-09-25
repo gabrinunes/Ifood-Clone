ff67b41722eb4f1bd23bfbb4ee63021f83092af2
Gabriel Cunha <gn_cunha@hotmail.com>
2019-09-24 23:43:45 -0300

    Listando empresas para usuário e search view

--- app/src/main/java/gabrielcunha/cursoandroidyoutubeapp/ifood/activity/activity/HomeActivity.java
+++ app/src/main/java/gabrielcunha/cursoandroidyoutubeapp/ifood/activity/activity/HomeActivity.java
@@ -3,6 +3,8 @@ package gabrielcunha.cursoandroidyoutubeapp.ifood.activity.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,15 +13,31 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import gabrielcunha.cursoandroidyoutubeapp.ifood.R;
+import gabrielcunha.cursoandroidyoutubeapp.ifood.activity.adapter.AdapterEmpresa;
 import gabrielcunha.cursoandroidyoutubeapp.ifood.activity.helper.ConfiguracaoFirebase;
+import gabrielcunha.cursoandroidyoutubeapp.ifood.activity.helper.UsuarioFirebase;
+import gabrielcunha.cursoandroidyoutubeapp.ifood.activity.model.Empresa;
 
 public class HomeActivity extends AppCompatActivity {
 
     private FirebaseAuth autenticacao;
     private MaterialSearchView searchView;
+    private RecyclerView recyclerEmpresas;
+    private AdapterEmpresa adapterEmpresa;
+    private List<Empresa> empresas = new ArrayList<>();
+    private DatabaseReference firebaseRef;
+    private String idUsuarioLogado ="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +45,8 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        firebaseRef = ConfiguracaoFirebase.getFirebase();
+        idUsuarioLogado = UsuarioFirebase.getIdUusuario();
         inicializarComponentes();
 
         //Configurações Toolbar
@@ -34,10 +54,89 @@ public class HomeActivity extends AppCompatActivity {
         toolbar.setTitle("Ifood");
         setSupportActionBar(toolbar);
 
+
+        //Configurar adapter
+
+        adapterEmpresa = new AdapterEmpresa(empresas);
+
+        //Configura RecyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerEmpresas.setHasFixedSize(true);
+        recyclerEmpresas.setLayoutManager(layoutManager);
+        recyclerEmpresas.setAdapter(adapterEmpresa);
+
+        //Configuração do search view
+        searchView.setHint("Pesquisar restaurantes");
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                pesquisarEmpresas(newText);
+                return true;
+            }
+        });
+
+        //Recuperar lista de empresas
+        recuperaEmpresas();
+
+    }
+
+    private void pesquisarEmpresas(String pesquisa) {
+        DatabaseReference empresasRef = firebaseRef
+                .child("empresas");
+        Query query = empresasRef.orderByChild("nome")
+                .startAt(pesquisa)
+                .endAt(pesquisa + "\uf8ff");
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                empresas.clear();
+
+                for(DataSnapshot ds: dataSnapshot.getChildren()){
+                    empresas.add(ds.getValue(Empresa.class));
+                }
+                adapterEmpresa.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void recuperaEmpresas() {
+        DatabaseReference empresasRef = firebaseRef
+                .child("empresas");
+        empresasRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                empresas.clear();
+
+                for(DataSnapshot ds:dataSnapshot.getChildren()){
+                    empresas.add(ds.getValue(Empresa.class));
+
+                }
+                adapterEmpresa.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
     private void inicializarComponentes() {
         searchView = findViewById(R.id.materialSearchView);
+        recyclerEmpresas = findViewById(R.id.recyclerEmpresas);
+
     }
 
     @Override
